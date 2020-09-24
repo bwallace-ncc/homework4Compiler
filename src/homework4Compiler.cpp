7f2cc9341be6b280d207173eb2c4497168d9c6e1
Brandon Wallace <btwallace@noctrl.edu>
2020-09-23 23:26:52 -0500

    started an outline

--- src/homework4Compiler.cpp
+++ src/homework4Compiler.cpp
@@ -7,6 +7,7 @@
 //============================================================================
 
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include<map>
 using namespace std;
@@ -21,23 +22,70 @@ private:
 	// other private methods
 public:
 
-LexAnalyzer(istream& infile);
+LexAnalyzer(istream& infile)
 // pre: parameter refers to open data file consisting of token and lexeme pairs
 //          i.e.  t_and and t_begin begin t_boolean boolean t_break break
 // post: tokenmap has been populated
+{
+	if(!infile)
+	{
+		cout << "error opening file" << endl;
+		exit(-1);
+	}
+	do //read data for lexeme/token pairs
+	{
+		//insert lexemes
+		string lexeme;
+		infile >> lexeme;
 
-void scanFile(istream& infile, ostream& outfile);
+		//insert token
+		string token;
+		infile >> token;
+		cout << lexeme << " " << token << endl;
+
+		//add to map
+		tokenmap.insert(pair<string, string>(lexeme, token));
+	}
+	while(!infile.eof());
+
+}
+
+void scanFile(istream& infile, ostream& outfile)
 // pre: 1st parameter refers to an open text file that contains source code in the
 //         language, 2nd parameter refers to an open empty output file
 	// post: the token and lexeme pairs for the given input file have been written to
 	//          the output file.  If there is an error, the incomplete token/lexeme pairs,
 // 	as well as an error message have printed to the output file.  A success or
 // 	fail message has printed to the console.
+{
+//read infile one line at a time
+
+//delimit to get "chunks"
+//identify chunks as keywords, types, or identifiers
+//add lexeme and token to vectors
+//continue search loop
+
+//print out to outputFile
+}
 };
 
 int main()
 {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	//inputting names of data files
+	string sourceCodeName;
+	cout << "Enter the name of the source code file: ";
+	cin >> sourceCodeName;
+
+	string lexemeDataFileName;
+	cout << "Enter the name of the lexeme data file: ";
+	cin >> lexemeDataFileName;
+	ifstream lexemeData(lexemeDataFileName);
+
+	string outputFileName;
+	cout << "Enter the name of the output file: ";
+	cin >> outputFileName;
+
+	LexAnalyzer lexAnalyzer(lexemeData);
 	return 0;
 }
 
