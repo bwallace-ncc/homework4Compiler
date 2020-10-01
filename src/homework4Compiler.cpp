d43e9621c92dd819c636cb03bddd796ee7efb4c4
Brandon Wallace <btwallace@noctrl.edu>
2020-10-01 16:30:52 -0500

    Final commit

--- src/homework4Compiler.cpp
+++ src/homework4Compiler.cpp
@@ -12,6 +12,7 @@
 #include<map>
 using namespace std;
 
+
 class LexAnalyzer
 {
 private:
@@ -20,6 +21,105 @@ private:
 	map<string, string> tokenmap;   // a map structure that maps lexemes to their
 					    // corresponding tokens for easy look up
 	// other private methods
+
+int inputString(istream& infile, ostream& outfile, string test, int i)
+// pre: 1st parameter refers to the current file being read
+//		2nd parameter refers to the output file that the tokens and lexemes are being put into
+//		3rd parameter refers to a string that is being tested for quotes to open a string lexeme
+//      4th parameter refers to the number of the character being tested for a quote
+	// post: if the character matches a quote, the string and its token will be written
+	//		 to their respective vectors
+{
+	string recordedString;
+	while(test[i]!='”' && test[i]!='"')//if there are more things in the string
+	{
+		if(test[i]=='\0')
+		{
+			getline(infile, test);
+			if(infile.eof())
+			{
+				lexemes.push_back(recordedString);
+				tokens.push_back("t_str");
+				for(int i=0; i<lexemes.size(); i++)
+				{
+					outfile << tokens.at(i) << " : " << lexemes.at(i) <<endl;
+				}
+				cout << "Error: string not closed. File read was unsuccessful." << endl;
+				exit(-2);
+			}
+			else
+			{
+				i=0;
+			}
+		}
+		recordedString.push_back(test[i]);
+		i++;
+	}
+	lexemes.push_back(recordedString);
+	tokens.push_back("t_str");
+	//i++; //add one to the index to get past the ending comma
+	return i;//show where we currently are on the line
+}
+
+int testLetters(string test, int i)
+// pre:	1st parameter refers to a string that is being tested for keyword or id lexemes
+//      2nd parameter refers to the number of the character being tested for a keyword or lexeme
+		// post: if the delimited string is a keyword the lexeme and a keyword token are put in the vectors
+		//		if the string is not a keyword the lexeme and an id token are put in the vectors
+{
+	string recordedString;
+	recordedString.push_back(test[i]);
+	while(isalnum(test[i+1]))
+	{
+		i++;
+		recordedString.push_back(test[i]);
+
+	}
+	if(tokenmap.count(recordedString)>0 && recordedString!="if")//match to keyword
+	{
+		lexemes.push_back(recordedString);
+		tokens.push_back(tokenmap[recordedString]);
+	}
+	else//otherwise it is an id
+	{
+		lexemes.push_back(recordedString);
+		tokens.push_back("t_id");
+	}
+	return i;
+}
+
+int testDigit(ostream& outfile, string test, int i)
+// pre:	1st parameter refers to the output file which the lexemes and tokens will be printed to
+//		2nd parameter refers to a string that is being tested for an integer
+//      3rd parameter refers to the number of the character being tested for an integer
+		// post: the integer has been found and output to the vectors with the int token
+{
+	string recordedInteger;
+	recordedInteger.push_back(test[i]);
+	while(isalnum(test[i+1]))
+	{
+		i++;
+		if(isalpha(test[i]))
+		{
+			lexemes.push_back(recordedInteger);
+			tokens.push_back("t_int");
+			for(int i=0; i<lexemes.size(); i++)
+			{
+				outfile << tokens.at(i) << " : " << lexemes.at(i) <<endl;
+			}
+			cout<< "Error: id cannot start with an integer, or integer and keyword are not delimited. File read was unsuccessful." << endl;
+			exit(-3);
+		}
+		else
+		{
+			recordedInteger.push_back(test[i]);
+		}
+	}
+	lexemes.push_back(recordedInteger);
+	tokens.push_back("t_int");
+	return i;
+}
+
 public:
 
 LexAnalyzer(istream& infile)
@@ -35,13 +135,12 @@ LexAnalyzer(istream& infile)
 	do //read data for lexeme/token pairs
 	{
 		//insert lexemes
-		string lexeme;
-		infile >> lexeme;
-
-		//insert token
 		string token;
 		infile >> token;
-		cout << lexeme << " " << token << endl;
+
+		//insert token
+		string lexeme;
+		infile >> lexeme;
 
 		//add to map
 		tokenmap.insert(pair<string, string>(lexeme, token));
@@ -58,59 +157,103 @@ void scanFile(istream& infile, ostream& outfile)
 // 	as well as an error message have printed to the output file.  A success or
 // 	fail message has printed to the console.
 {
-//read infile one line at a time
-	if(!infile)
+	if(!infile)//check if infile exists
 	{
 		cout << "error opening file" << endl;
 		exit(-1);
 	}
-	do //read data for lexeme/token pairs
+	string line;
+	while(getline(infile, line))//read next line of file
 	{
-		cout <<"in scanFile Loop"<< endl;
-		//insert lexemes
-		string line;
-		getline(infile, line);
-		cout << "line is" << line << endl;
-
-		//delimit to get "chunks"
-		string word;
 		for(int i=0; i<line.size(); i++)
 		{
-		cout << "in word loop" << endl;
-		word.push_back(line[i]);
-		cout << word << endl;
-		}
+			//setup our test variables
+			bool matchFound=false;
+			string testSingleChar;
+			testSingleChar.push_back(line[i]);
+			//test the one and two character operators
+			if(i+1<line.size())
+			{
+				string testDoubleChar;
+				testDoubleChar.push_back(line[i]);
+				testDoubleChar.push_back(line[i+1]);
+				if(tokenmap.count(testDoubleChar)>0)//test double char
+				{
+					//add lexemes and tokens to vectors if you find a match
+					lexemes.push_back(testDoubleChar);
+					tokens.push_back(tokenmap[testDoubleChar]);
+					matchFound=true;
+				}
+			}
+			if(!matchFound)
+			{
+				if(tokenmap.count(testSingleChar)>0)//test single char
+				{
+					//add lexemes and tokens to vectors if you find a match
+					lexemes.push_back(testSingleChar);
+					tokens.push_back(tokenmap[testSingleChar]);
+					matchFound=true;
+				}
+			}
+			if(!matchFound)
+			{
+				if(line[i] == '“' || line[i] == '"')//test for strings
+				{
+					i++;//start reading at the first character of the string
+					int index = inputString(infile, outfile, line, i);
+					i = index;
+					matchFound = true;
+				}
+			}
+			if(!matchFound)
+			{
+				if(isdigit(line[i]))//test for integer
+				{
+					int index = testDigit(outfile, line, i);
+					i = index;
+					matchFound = true;
+				}
+			}
+			if(!matchFound)
+			{
+				if(isalpha(line[i]))// test for keyword or id
+				{
+					int index = testLetters(line, i);
+					i = index;
+				}
+			}
+		}//end for loop
+	}//end while loop
+	//print to outputFile
+	for(int i=0; i<lexemes.size(); i++)
+	{
+		outfile << tokens.at(i) << " : " << lexemes.at(i) <<endl;
 	}
-	while(!infile.eof());
-
-
-//identify chunks as keywords, types, or identifiers
-//add lexeme and token to vectors
-//continue search loop
-
-//print out to outputFile
-
+	cout << "Entire file was successfully read." << endl;//console message
 }
+
 };
+
 
 int main()
 {
 	//inputting names of data files
 	string sourceCodeName;
-	//cout << "Enter the name of the source code file: ";
-	//cin >> sourceCodeName;
-	ifstream sourceCode("sourceCodeInput.txt");
+	cout << "Enter the name of the source code file: ";
+	cin >> sourceCodeName;
+	ifstream sourceCode(sourceCodeName);
 
 	string lexemeDataFileName;
-	//cout << "Enter the name of the lexeme data file: ";
-	//cin >> lexemeDataFileName;
-	ifstream lexemeData("tokenlexemedata.txt");
+	cout << "Enter the name of the lexeme data file: ";
+	cin >> lexemeDataFileName;
+	ifstream lexemeData(lexemeDataFileName);
 
 	string outputFileName;
-	//cout << "Enter the name of the output file: ";
-	//cin >> outputFileName;
-	ofstream output("outputFile.txt");
+	cout << "Enter the name of the output file: ";
+	cin >> outputFileName;
+	ofstream output(outputFileName);
 
+	//execute the words!
 	LexAnalyzer lexAnalyzer(lexemeData);
 	lexAnalyzer.scanFile(sourceCode, output);
 	return 0;
